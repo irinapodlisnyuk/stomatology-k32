10e6e464b02384533b002198b31698faa3fe8e3d
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-07-14 11:11:05 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["192.168.0.138"],
+  sassOptions: {
+    silenceDeprecations: ["import", "legacy-js-api"],
+  },
 };
 
 export default nextConfig;
