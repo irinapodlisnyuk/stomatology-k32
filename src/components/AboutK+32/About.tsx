1fbbc8d5b37315f8dcfd4b2d97595039a85dc775
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-07-17 19:24:34 +0300

    about info

--- src/components/AboutK+32/About.tsx
+++ src/components/AboutK+32/About.tsx
@@ -7,19 +7,40 @@ export default function AboutK32() {
       <div className="container">
         <div className={styles.about__wrapper}>
           <div className={styles["about__wrapper-info"]}>
-            <h2 className={styles.info__title}>О&nbsp;компании К+32</h2>
-            <p className={styles["info__subtitle"]}>
-              Мы&nbsp;заботимся о&nbsp;здоровье ваших зубов.
+            <h2 className={styles.about__title}>О&nbsp;стоматологии К+32</h2>
+            <p className={styles["about__subtitle"]}>
+              Мы&nbsp;Заботимся о&nbsp;Здоровье Ваших Зубов
             </p>
-            <p className={styles.info__text}></p>
+            <p className={styles.about__text}>
+              Наша стоматологическая клиника объединяет передовые технологии,
+              многолетний опыт врачей и&nbsp;искреннюю заботу о&nbsp;каждом
+              пациенте. Мы&nbsp;создаем комфортные условия для лечения
+              и&nbsp;помогаем обрести уверенность в&nbsp;вашей улыбке.
+            </p>
 
-            <div className={styles["info__actions"]}>
+            <div className={styles["about__actions"]}>
               <Link href="/BookAppointment" className="btn btn--about">
                 Узнать больше
               </Link>
             </div>
           </div>
-          <div className={styles["about__wrapper-image"]}></div>
+          <div className={styles["about__wrapper-image"]}>
+            <picture className={styles.about__picture}>
+              <source
+                srcSet="/image/about/about.webp 1x, /image/about/about@2x.webp 2x"
+                type="image/webp"
+              />
+              <source
+                srcSet="/image/about/about.jpg 1x, /image/about/about@2x.jpg 2x"
+                type="image/jpg"
+              />
+              <img
+                src="/image/about/about.jpg"
+                alt="Прием в  Клиника +32"
+                className={styles.hero__bg}
+              />
+            </picture>
+          </div>
         </div>
       </div>
     </section>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,15 +7,15 @@ export default function Hero() {
       <div className="container">
         <picture className={styles.hero__picture}>
           <source
-            srcSet="/image/hero-bg.webp 1x, /image/hero-bg@2x.webp 2x"
+            srcSet="/image/hero/hero-bg.webp 1x, /image/hero/hero-bg@2x.webp 2x"
             type="image/webp"
           />
           <source
-            srcSet="/image/hero-bg.jpg 1x, /image/hero-bg@2x.jpg 2x"
+            srcSet="/image/hero/hero-bg.jpg 1x, /image/hero-bg@2x.jpg 2x"
             type="image/jpg"
           />
           <img
-            src="/image/hero-bg.jpg"
+            src="/image/hero/hero-bg.jpg"
             alt="Современная стоматология Клиника +32"
             className={styles.hero__bg}
           />
