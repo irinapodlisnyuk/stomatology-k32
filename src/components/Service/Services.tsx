a9342a489d4b2589b8fd655ec6fdd71b763a330e
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-07-18 19:11:59 +0300

    scss service

--- src/components/Service/Services.tsx
+++ src/components/Service/Services.tsx
@@ -9,12 +9,12 @@ export default function Services() {
         <div className={styles.services__wrapper}>
           <div className={styles["services__header"]}>
             <h2 className={styles["services__header-title"]}>
-              Комплексные стоматологические услуги
+              Cтоматологические услуги
             </h2>
-            <p className={styles["services__subtitle"]}>
-              Забота о&nbsp;каждом аспекте вашей улыбки.
+            <p className={styles["services__header-subtitle"]}>
+              Забота о&nbsp;каждом аспекте вашей улыбки
             </p>
-            <p className={styles.services__text}>
+            <p className={styles["services__header-text"]}>
               Здоровье зубов определяет качество жизни, поэтому мы&nbsp;создали
               пространство, где лечение проходит абсолютно безболезненно
               и&nbsp;комфортно. Индивидуальный план лечения, современное
@@ -25,100 +25,6 @@ export default function Services() {
           </div>
 
           <ServicesList/>
-
-          {/* <ul className={styles["services__list"]}>
-            <li className={styles["services__item"]}>
-              <div className={styles["services__item-image"]}>
-                <picture className={styles["services__picture"]}>
-                  <source
-                    srcSet="/image/services/сosmetic-dentistry.webp 1x, /image/services/сosmetic-dentistry@2x.webp 2x"
-                    type="image/webp"
-                  />
-                  <source
-                    srcSet="/image/services/сosmetic-dentistry.jpg 1x, /image/services/сosmetic-dentistry@2x.jpg 2x"
-                    type="image/jpg"
-                  />
-                  <img
-                    src="/image/services/сosmetic-dentistry.jpg"
-                    alt="Услуги в К+32"
-                    className={styles["services__picture-img"]}
-                  />
-                </picture>
-                <span  className={styles["services__item-title"]}>
-                    отбеливание
-                </span>
-              </div>
-            </li>
-
-                 <li className={styles["services__item"]}>
-              <div className={styles["services__item-image"]}>
-                <picture className={styles["services__picture"]}>
-                  <source
-                    srcSet="/image/services/preventive-care.webp 1x, /image/services/preventive-care@2x.webp 2x"
-                    type="image/webp"
-                  />
-                  <source
-                    srcSet="/image/services/preventive-care.jpg 1x, /image/services/preventive-care@2x.jpg 2x"
-                    type="image/jpg"
-                  />
-                  <img
-                    src="/image/services/preventive-care.jpg"
-                    alt="Услуги в К+32"
-                    className={styles["services__picture-img"]}
-                  />
-                </picture>
-                <span  className={styles["services__item-title"]}>
-                   терапия
-                </span>
-              </div>
-            </li>
-
-                 <li className={styles["services__item"]}>
-              <div className={styles["services__item-image"]}>
-                <picture className={styles["services__picture"]}>
-                  <source
-                    srcSet="/image/services/orthopedics.webp 1x, /image/services/orthopedics@2x.webp 2x"
-                    type="image/webp"
-                  />
-                  <source
-                    srcSet="/image/services/orthopedics.jpg 1x, /image/services/orthopedics@2x.jpg 2x"
-                    type="image/jpg"
-                  />
-                  <img
-                    src="/image/services/orthopedics.jpg"
-                    alt="Услуги в К+32"
-                    className={styles["services__picture-img"]}
-                  />
-                </picture>
-                <span  className={styles["services__item-title"]}>
-                    ортопедия
-                </span>
-              </div>
-            </li>
-
-                 <li className={styles["services__item"]}>
-              <div className={styles["services__item-image"]}>
-                <picture className={styles["services__picture"]}>
-                  <source
-                    srcSet="/image/services/implantation.webp 1x, /image/services/implantation@2x.webp 2x"
-                    type="image/webp"
-                  />
-                  <source
-                    srcSet="/image/services/implantation.jpg 1x, /image/services/implantation@2x.jpg 2x"
-                    type="image/jpg"
-                  />
-                  <img
-                    src="/image/services/implantation.jpg"
-                    alt="Услуги в К+32"
-                    className={styles["services__picture-img"]}
-                  />
-                </picture>
-                <span  className={styles["services__item-title"]}>
-                    имплантация
-                </span>
-              </div>
-            </li>
-          </ul> */}
 
           <div className={styles["services__actions"]}>
             <Link href="/services" className="btn btn--services">
