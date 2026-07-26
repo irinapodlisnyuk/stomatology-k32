13c40a748095650be1a70d3cf4a1f7e65f0d1aea
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-07-26 12:14:37 +0300

    fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,28 @@
 import type { Metadata } from "next";
+import { Poppins, Manrope, Montserrat } from "next/font/google";
 import "@/assets/styles/main.scss";
 import HeaderComponent from "@/components/Header/Header";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins", // Имя CSS-переменной
+  display: "swap", // Текст виден сразу, предотвращает блокировку отрисовки
+});
+
+const manrope = Manrope({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-text",
+  display: "swap",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin", "cyrillic"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-heading",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title:
@@ -33,7 +55,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
+    <html
+      lang="ru"
+      className={`${poppins.variable} ${manrope.variable} ${montserrat.variable}`}
+    >
       <body>
         <div className="root-wrapper">
           <HeaderComponent />
