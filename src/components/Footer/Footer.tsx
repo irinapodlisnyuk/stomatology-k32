be318821b972745ce9d6c43ed6c5aaef90202287
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-07-28 10:50:05 +0300

    footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import styles from "./Footer.module.scss";
+
+export default function Footer() {
+
+
+
+
+  return (
+    <section className={styles.footer}>
+      <div className="container">
+        <div className={styles.footer__wrapper}>
+          <div className={styles["footer__header"]}>
+          
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+}
