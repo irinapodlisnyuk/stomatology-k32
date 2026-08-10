e2f7b87ea9c6826e0b4219e1952d4a0f7b8b5698
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-08-10 15:24:48 +0300

    hero a

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import styles from "./hero.module.scss";
 import { useModals } from "@/components/context/ModalContext";
+import Image from "next/image";
 
 interface HeroProps {
   title?: string;
@@ -25,70 +26,24 @@ export default function Hero({
 }: HeroProps) {
   const { openAppointment } = useModals();
 
-  const mobSuffix = "-mob";
-  const tabSuffix = "-tab";
-
   const modifierClass = pageType !== "home" ? styles[`hero--${pageType}`] : "";
 
-  const imageSizes =
-    pageType === "call"
-      ? "(max-width: 767px) 360px, (max-width: 1199px) 768px, 100vw"
-      : "(max-width: 767px) 360px, (max-width: 1199px) 100vw, 100vw";
   return (
     <section className={`${styles.hero} ${modifierClass}`}>
       <div className="container">
-        <picture className={styles.hero__picture}>
-          {/* 1. Мобильные устройства (до 767px) */}
-          <source
-            media="(max-width: 767px)"
-            srcSet={`${imageFolder}/${imageName}${mobSuffix}.webp 768w, ${imageFolder}/${imageName}${mobSuffix}@2x.webp 1536w`}
-            sizes={imageSizes}
-            type="image/webp"
-          />
-          <source
-            media="(max-width: 767px)"
-            srcSet={`${imageFolder}/${imageName}${mobSuffix}.jpg 768w, ${imageFolder}/${imageName}${mobSuffix}@2x.jpg 1536w`}
-            sizes={imageSizes}
-            type="image/jpeg"
-          />
-
-          {/* 2. Планшеты (до 1199px) */}
-          <source
-            media="(max-width: 1199px)"
-            srcSet={`${imageFolder}/${imageName}${tabSuffix}.webp 1024w, ${imageFolder}/${imageName}${tabSuffix}@2x.webp 2048w`}
-            sizes={imageSizes}
-            type="image/webp"
-          />
-          <source
-            media="(max-width: 1199px)"
-            srcSet={`${imageFolder}/${imageName}${tabSuffix}.jpg 1024w, ${imageFolder}/${imageName}${tabSuffix}@2x.jpg 2048w`}
-            sizes={imageSizes}
-            type="image/jpeg"
-          />
-
-          {/* 3. Десктоп (от 1200px) */}
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`${imageFolder}/${imageName}.webp 1440w, ${imageFolder}/${imageName}@2x.webp 2880w`}
-            sizes={imageSizes}
-            type="image/webp"
-          />
-          <source
-            media="(min-width: 1200px)"
-            srcSet={`${imageFolder}/${imageName}.jpg 1440w, ${imageFolder}/${imageName}@2x.jpg 2880w`}
-            sizes={imageSizes}
-            type="image/jpeg"
-          />
-
-          <img
+        {/* ИДЕАЛЬНОЕ РЕШЕНИЕ ДЛЯ LCP И NEXT.JS 16 */}
+        <div className={styles.hero__picture}>
+          <Image
             src={`${imageFolder}/${imageName}.webp`}
             alt={altText.replace(/&nbsp;/g, " ")}
             className={`${styles.hero__img || ""} ${styles.hero__bg || ""}`}
-            fetchPriority="high"
-            loading="eager"
-            decoding="sync"
+            fill 
+            style={{ objectFit: "cover" }}
+          
+            priority={true}
+            loading="eager" 
           />
-        </picture>
+        </div>
 
         <div className={styles.hero__wrapper}>
           <h2
