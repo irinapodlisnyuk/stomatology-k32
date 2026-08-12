d12fe3f2217fbf7a212079c10479a95265013ca0
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-08-12 20:30:08 +0300

    navigation

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -8,7 +8,7 @@ export const navigationItems: MenuItem[] = [
   { key: 0, path: "/", label: "Главная" },
   { key: 1, path: "/about-us", label: "О нас" },
   { key: 2, path: "/service", label: "Услуги" },
-  { key: 3, path: "/price", label: "Наш Прайс" },
+  { key: 3, path: "/price", label: "Наш прайс" },
   { key: 4, path: "/blog", label: "Блог" },
   { key: 5, path: "/contact", label: "Связаться с нами" },
    
